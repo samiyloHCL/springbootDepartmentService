59c26d8d4b4929d90d44979bf423dcd8c7a54b5a
Samiylo <skryshu@gmail.com>
2021-02-10 13:04:33 -0600

    add Async Bean and Scheduling

--- src/main/java/com/example/demo/SpringBootDepartmentMicroserviceApplication.java
+++ src/main/java/com/example/demo/SpringBootDepartmentMicroserviceApplication.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import java.util.concurrent.Executor;
+
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootDepartmentMicroserviceApplication {
 
@@ -10,6 +19,19 @@ public class SpringBootDepartmentMicroserviceApplication {
 		SpringApplication.run(SpringBootDepartmentMicroserviceApplication.class, args);
 		
 		
+	}
+	
+	@Bean
+	public Executor taskExecutor() {
+		
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		
+		executor.setCorePoolSize(1);
+		executor.setMaxPoolSize(1);
+		executor.setQueueCapacity(500);
+		executor.initialize();
+		
+		return executor;
 	}
 
 }
