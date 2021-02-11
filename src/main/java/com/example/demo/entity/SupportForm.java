f96162454d4c7b580d00e20917774aab6b91c654
Samiylo <skryshu@gmail.com>
2021-02-11 11:20:06 -0600

    create support form

---
+++ src/main/java/com/example/demo/entity/SupportForm.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+public class SupportForm {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String gender;
+	private String membership;
+	private String content;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMembership() {
+		return membership;
+	}
+	public void setMembership(String membership) {
+		this.membership = membership;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String toString() {
+		
+		return "Name: " + name + "/n Email: " + email + "/n Gender: " + gender + "/n Membership: " + membership;
+	}
+
+}
