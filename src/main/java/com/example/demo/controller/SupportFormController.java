f6e5410897dfb9ebf97f9e0d51db3afcb554c92d
Samiylo <skryshu@gmail.com>
2021-02-11 11:48:33 -0600

    add routes to support

--- src/main/java/com/example/demo/controller/SupportFormController.java
+++ src/main/java/com/example/demo/controller/SupportFormController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.SupportForm;
 
@@ -23,6 +25,14 @@ public class SupportFormController {
 		
 		return "support";
 		
+	}
+	
+	@PostMapping("/support")
+	public String submitComplaint(@ModelAttribute SupportForm supportDetails, Model model) {
+		
+		model.addAttribute("supportDetails", supportDetails);
+		
+		return "submit";
 	}
 
 }
