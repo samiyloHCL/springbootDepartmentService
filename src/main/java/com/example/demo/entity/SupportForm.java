b790d715761e00475beba66dbf0d14a7ad9b224d
Samiylo <skryshu@gmail.com>
2021-02-16 11:37:27 -0600

    add validation to SupportForm

--- src/main/java/com/example/demo/entity/SupportForm.java
+++ src/main/java/com/example/demo/entity/SupportForm.java
@@ -1,12 +1,32 @@
 package com.example.demo.entity;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class SupportForm {
 	
+	@NotNull
+	@Min(value=10000, message = "customer ID should be >= 10000")
 	private Long id;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
 	private String name;
+	
+	@NotEmpty
+	@Email
 	private String email;
+	
+	@NotNull
 	private String gender;
+	
+	@NotNull
 	private String membership;
+	
+	@NotNull
 	private String content;
 	public Long getId() {
 		return id;
