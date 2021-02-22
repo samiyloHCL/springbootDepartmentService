465c58cde857972621a10dafe55abe944335c00c
Samiylo <skryshu@gmail.com>
2021-02-22 12:03:58 -0600

    initiate Quote Microservice

---
+++ src/main/java/com/example/demo/entity/Quote.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+public class Quote {
+	
+	private String value;
+	private String by;
+	
+	public Quote() {}
+	public Quote(String value, String by) {
+		this.value = value;
+		this.by = by;
+	}
+	
+	
+
+}
