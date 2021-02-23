815a03cbed967979393019c463f3c1f662f1d69a
Samiylo <skryshu@gmail.com>
2021-02-23 16:46:32 -0600

    update service

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Quote;
+import com.example.demo.service.FinanceService;
 import com.example.demo.service.QuoteService;
 
 @RestController
@@ -42,6 +46,19 @@ public class DepartmentController {
 	public String error() {
 		
 		return "You are now on the Department Info";
+	}
+	
+	//YahooFinance API
+	@RequestMapping("/finance")
+	public String finance() throws Exception, InterruptedException {
+		
+		FinanceService service = new FinanceService();
+			HttpResponse<String> response = service.getAutoComplete();
+			
+			System.out.println(response.body());
+			String output = response.body();
+			return output;
+	
 	}
 
 }
--- src/main/java/com/example/demo/service/FinanceService.java
+++ src/main/java/com/example/demo/service/FinanceService.java
@@ -1,5 +1,29 @@
 package com.example.demo.service;
 
-public class FinanceService {
+import java.util.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+import com.example.demo.entity.FinanceKey;
+
+public class FinanceService {
+	
+	FinanceKey apiKey = new FinanceKey();
+		
+	
+	public HttpResponse<String> getAutoComplete() throws IOException, InterruptedException {
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=apple&region=US"))
+				.header("x-rapidapi-key", "6940820188msh40f92ab7c895240p158dedjsn045dc77c4eeb")
+				.header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		return response;
+		
+	}
+	
 }
