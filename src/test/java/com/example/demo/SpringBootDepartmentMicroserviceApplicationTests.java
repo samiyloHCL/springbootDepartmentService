a6c8d05fcf93f5dee7ca5bf4342fa2eb0695cd49
Samiylo <skryshu@gmail.com>
2021-02-23 11:17:26 -0600

    add Junit test

--- src/test/java/com/example/demo/SpringBootDepartmentMicroserviceApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootDepartmentMicroserviceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.entity.Quote;
+import com.example.demo.service.QuoteService;
+
 @SpringBootTest
 class SpringBootDepartmentMicroserviceApplicationTests {
+	
+	QuoteService quoteService;
+	
+	@BeforeAll
+	public void before() {
+		this.quoteService = new QuoteService();
+	}
 
 	@Test
 	void contextLoads() {
+		this.quoteService.addQuote(new Quote("ABC", "D"));
+		assertEquals("ABC", this.quoteService.getQuote().getValue());
 	}
 
 }
