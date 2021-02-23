586ac86da23019e4943e328dc34a367a24647f67
Samiylo <skryshu@gmail.com>
2021-02-23 16:46:02 -0600

    fix key

--- src/main/java/com/example/demo/entity/FinanceKey.java
+++ src/main/java/com/example/demo/entity/FinanceKey.java
@@ -2,6 +2,27 @@ package com.example.demo.entity;
 
 public class FinanceKey {
 	
-	private String apiKey "hello";
+	private String RapidApp = "default-application_5078926";
+	private String RapidAPI = "";
+	private String RapidHost = "apidojo-yahoo-finance-v1.p.rapidapi.com";
+	
+	
+	
+	public String getRapidApp() {
+		return RapidApp;
+	}
+	
+	public String getRapidAPI() {
+		return RapidAPI;
+	}
+	
+	public String getRapidHost() {
+		return RapidHost;
+	}
+	
+	
+	
+	
+	
 	
 }
